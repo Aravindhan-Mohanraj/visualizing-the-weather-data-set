1c2811c0b9b2527053100a76fb8835e19856a1d0
Aravindhan <aravindhan405@gmail.com>
2020-11-17 12:56:20 +0530

    Code file created.

---
+++ code.py
@@ -0,0 +1,126 @@
+# --------------
+# Import the required Libraries
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import calendar
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# Generate a line chart that visualizes the readings in the months
+
+def line_chart(df,period,col):
+    """ A line chart that visualizes the readings in the months
+    
+    This function accepts the dataframe df ,period(day/month/year) and col(feature), which plots the   aggregated value of the feature based on the periods. Ensure the period labels are properly named.
+    
+    Keyword arguments:
+    df - Pandas dataframe which has the data.
+    period - Period of time over which you want to aggregate the data
+    col - Feature of the dataframe
+    
+    """
+    df = df.reset_index()
+    df['month'] = df['Date/Time'].dt.month
+    month_wise = df.groupby(["month"])[col].mean()
+    xlabels = []
+    for i in range(1, 13):
+        xlabels.append(calendar.month_name[i])
+    print(xlabels)
+
+    print(month_wise)
+    plt.plot(month_wise)
+    plt.xticks(np.arange(1, 13), xlabels, rotation=90)
+    plt.show()
+
+# Function to perform univariate analysis of categorical columns
+def plot_categorical_columns(df):
+    """ Univariate analysis of categorical columns
+    
+    This function accepts the dataframe df which analyzes all the variable in the data and performs the univariate analysis using bar plot.
+    
+    Keyword arguments:
+    df - Pandas dataframe which has the data.
+    
+    """
+    
+
+
+
+
+
+
+
+
+# Function to plot continous plots
+def plot_cont(df,plt_typ):
+    """ Univariate analysis of Numerical columns
+    
+    This function accepts the dataframe df, plt_type(boxplot/distplot) which analyzes all the variable in the data and performs the univariate analysis using boxplot or distplot plot.
+    
+    Keyword arguments:
+    df - Pandas dataframe which has the data.
+    plt_type - type of plot through which you want to visualize the data
+    
+    """
+    
+    
+
+
+
+
+
+
+
+# Function to plot grouped values based on the feature
+def group_values(df,col1,agg1,col2):
+    """ Agrregate values by grouping
+    
+    This function accepts a dataframe, 2 column(feature) and aggregated function(agg1) which groupby the dataframe based on the column and plots the bar plot.
+   
+    Keyword arguments:
+    df - Pandas dataframe which has the data.
+    col1 - Feature of the dataframe on which values will be aggregated.
+    agg1 - Dictionary of aggregate functions with feature as the key and func as the value
+    col2 - Feature of the dataframe to be plot against grouped data.
+    
+    Returns:
+    grouping - Dataframe with all columns on which it is grouped on.
+    """
+    
+    
+
+
+
+
+# Read the Data and pass the parameter as parse_dates=True, index_col='Date/Time'
+
+weather_df = pd.read_csv(path,parse_dates=True, index_col='Date/Time')
+print(weather_df.head())
+
+# Lets try to generate a line chart that visualizes the temperature readings in the months.
+# Call the function line_chart() with the appropriate parameters.
+line_chart(weather_df,'01/01/2012','Temp (C)')
+
+
+# Now let's perform the univariate analysis of categorical features.
+# Call the "function plot_categorical_columns()" with appropriate parameters.
+
+
+
+# Let's plot the Univariate analysis of Numerical columns.
+# Call the function "plot_cont()" with the appropriate parameters to plot distplot
+
+
+
+# Call the function "plot_cont()" with the appropriate parameters to plot boxplot
+
+
+# Groupby the data by Weather and plot the graph of the mean visibility during different weathers. Call the function group_values to plot the graph.
+# Feel free to try on diffrent features and aggregated functions like max, min.
+
+
+
+
